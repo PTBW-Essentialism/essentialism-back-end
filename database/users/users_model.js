dc755c187373ad19b7cf00133f7502a57816d542
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-05-28 00:04:50 -0500

    tweaked model

--- database/users/users_model.js
+++ database/users/users_model.js
@@ -3,22 +3,22 @@ const db = require("../config");
 
 async function add(user) {
     user.password = await bcrypt.hash(user.password, 14);
-    const [id] = await db("users").insert(user).returning("id");
+    const [id] = await db("Users").insert(user).returning("id");
     return findById(id);
 }
 
 function find() {
-    return db("users").select("id", "username");
+    return db("Users").select("id", "username");
 }
 
 function findBy(filter) {
-    return db("users")
+    return db("Users")
         .select("id", "username", "password", "email", "firstName", "lastName")
         .where(filter);
 }
 
 function findById(id) {
-    return db("users")
+    return db("Users")
         .where({ id })
         .select("id", "username", "password", "email", "firstName", "lastName")
         .first();
