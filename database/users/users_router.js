1846c9253be6e8f355ba3644f8768d50d1333f9a
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-05-28 14:38:31 -0500

    fixed final endpoint

--- database/users/users_router.js
+++ database/users/users_router.js
@@ -126,7 +126,7 @@ router.post("/:id/initiatives", validateUserID(), (req, res, next) => {
 
 router.delete("/:id/initiatives/:InId", validateUserID(), (req, res, next) => {
     try {
-        Dashboard.removeInitiative(req.params.InId).then(
+        Dashboard.removeInitiative(req.params.id, req.params.InId).then(
             res.status(200).json({ message: "Successfully deleted" })
         );
     } catch (err) {
