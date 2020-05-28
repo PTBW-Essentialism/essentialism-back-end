158c483501b7e558d92dbb7c0ed0f9fa519e5b96
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-05-27 20:56:19 -0500

    removed clean seed, added promise deletion

--- database/seeds/04_UserValues.js
+++ database/seeds/04_UserValues.js
@@ -1,19 +1,23 @@
 exports.seed = async function (knex) {
-    await knex("UserValues").insert([
-        { userId: 1, valuesId: 1 },
-        { userId: 1, valuesId: 2 },
-        { userId: 1, valuesId: 3 },
-        { userId: 2, valuesId: 1 },
-        { userId: 2, valuesId: 2 },
-        { userId: 2, valuesId: 3 },
-        { userId: 3, valuesId: 1 },
-        { userId: 3, valuesId: 2 },
-        { userId: 3, valuesId: 3 },
-        { userId: 4, valuesId: 1 },
-        { userId: 4, valuesId: 2 },
-        { userId: 4, valuesId: 3 },
-        { userId: 5, valuesId: 1 },
-        { userId: 5, valuesId: 2 },
-        { userId: 5, valuesId: 3 },
-    ]);
+    return knex("UserValues")
+        .del()
+        .then(
+            await knex("UserValues").insert([
+                { userId: 1, valuesId: 1 },
+                { userId: 1, valuesId: 2 },
+                { userId: 1, valuesId: 3 },
+                { userId: 2, valuesId: 1 },
+                { userId: 2, valuesId: 2 },
+                { userId: 2, valuesId: 3 },
+                { userId: 3, valuesId: 1 },
+                { userId: 3, valuesId: 2 },
+                { userId: 3, valuesId: 3 },
+                { userId: 4, valuesId: 1 },
+                { userId: 4, valuesId: 2 },
+                { userId: 4, valuesId: 3 },
+                { userId: 5, valuesId: 1 },
+                { userId: 5, valuesId: 2 },
+                { userId: 5, valuesId: 3 },
+            ])
+        );
 };
