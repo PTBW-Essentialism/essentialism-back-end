a83b66e6ef0c6902e04b88bb9a2b3d5087cc4a7a
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-05-28 11:21:56 -0500

    fixing truncate

--- database/seeds/00_clear.js
+++ database/seeds/00_clear.js
@@ -1,9 +1,9 @@
 exports.seed = async function (knex) {
     // if (process.env.NODE_ENV != "production") {
-    await knex("UserValues").truncate();
+    await knex("Users").truncate();
     await knex("Initiatives").truncate();
     await knex("Values").truncate();
-    await knex("Users").truncate();
+    await knex("UserValues").truncate();
     // }
 };
 // exports.seed = async function (knex) {
