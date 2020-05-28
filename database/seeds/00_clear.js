ed5abc3b7cc87d81d3ac032ecc618eadf3329b16
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-05-28 11:37:32 -0500

    added cap

--- database/seeds/00_clear.js
+++ database/seeds/00_clear.js
@@ -1,12 +1,11 @@
 exports.seed = async function (knex) {
     // if (process.env.NODE_ENV != "production") {
-    // await knex.raw("SET foreign_key_checks = 0");
-    await knex.raw("TRUNCATE TABLE Users RESTART IDENTITY CASCADE");
+    await knex.raw("SET FOREIGN_KEY_CHECKS = 0");
     await knex("Initiatives").truncate();
     await knex("UserValues").truncate();
     await knex("Values").truncate();
     await knex("Users").truncate();
-    await knex.raw("SET foreign_key_checks = 1");
+    await knex.raw("SET FOREIGN_KEY_CHECKS = 1");
 
     // }
 };
