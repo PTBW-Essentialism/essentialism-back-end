04f08074970de28ebb87b3b4cc6e62b7b45150f1
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-05-28 19:16:44 -0500

    added cors

--- database/auth/auth_router.js
+++ database/auth/auth_router.js
@@ -4,7 +4,7 @@ const Users = require("../users/users_model");
 const jwt = require("jsonwebtoken");
 
 router.all("/", function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); //
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); //
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
 });
